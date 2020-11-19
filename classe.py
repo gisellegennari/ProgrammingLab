edb05b1b8bf5f340279f1e651f5444486f0ae012
gisellegennari <74489093+gisellegennari@users.noreply.github.com>
2020-11-19 16:20:12 +0100

    Create classe.py

---
+++ classe.py
@@ -0,0 +1,26 @@
+class primaclasse:
+  "Ho creato la prima classe" # __doc__
+  pass
+
+print(primaclasse.__doc__)
+
+# La mia seconda classe con una funzione
+# definita dentro la classe
+class secondaclasse:
+  "Ho creato la seconda classe" # __doc__
+
+  def fun(self):                # funzione
+    print('Stampa di prova')
+  pass
+
+print(secondaclasse.__doc__)
+
+# Lo studente
+class secondaclasse:
+  "Ho creato la seconda classe" # __doc__
+
+  def fun(self):                # funzione
+    print('Stampa di prova')
+  pass
+
+print(secondaclasse.__doc__)
