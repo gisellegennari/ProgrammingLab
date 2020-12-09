c7e1da268834f6ffa49cc3637f3f53cf2a159ad7
gisellegennari <74489093+gisellegennari@users.noreply.github.com>
2020-12-09 15:45:16 +0100

    Create funzioni.py

---
+++ funzioni.py
@@ -0,0 +1,13 @@
+class Funzioni 
+   def __init__(self: a<=b<=c)
+      a<=b<=c == a<=b<=c
+      
+      if(a<=b,c>b)
+      elise(a>b, c<a and c>b)
+      else
+      
+        if sum(a+b == d and d<b)
+        else dif(a+b < c)
+        return = 0
+        
+        stamp (def
